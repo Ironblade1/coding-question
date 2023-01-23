215a8dcc064a5c64ca7a99afa89b6bb2257b9ef8
Ironblade1 <83993262+Ironblade1@users.noreply.github.com>
2023-01-23 15:59:05 +0530

    Time: 7 ms (74.40%), Space: 10.8 MB (60.36%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+
+    void solve(vector<int>& cand, int t,vector<vector<int>> &res, vector<int> &ans,int i){
+
+        if(t==0) {
+            res.push_back(ans);
+            return ;
+        }
+        while(i<cand.size() && t-cand[i]>=0){
+            ans.push_back(cand[i]);
+            solve(cand,t-cand[i],res,ans,i);
+            
+            ans.pop_back();
+            i++;
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> res;
+        vector<int> ans;
+        solve(candidates,target,res,ans,0);
+        return res; 
+    }
+};
