f93016f319b896e351f0d67f9827035685509170
Ironblade1 <83993262+Ironblade1@users.noreply.github.com>
2023-01-27 11:56:59 +0530

    Time: 36 ms (50.33%), Space: 21 MB (6.38%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -35,12 +35,30 @@ public:
         return dp[0][0];
     }
     
+    int solveSO(vector<int> &sat){
+        int n = sat.size();
+        vector<int> curr(n+1,0);
+        vector<int> next(n+1,0);
+                
+        for(int index=n-1;index>=0;index--){
+            for(int time =index;time>=0;time--){
+                int inc = sat[index]*(time+1)+ next[time+1];
+                int exc = 0 + next[time];
+                curr[time] = max(inc, exc);
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+    
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
         // return solve(satisfaction, 0, 0);
         
         int n = satisfaction.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solveMem(satisfaction,0,0,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return solveMem(satisfaction,0,0,dp);
+        
+        return solveTab(satisfaction);
     }
 };
