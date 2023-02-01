a7b0eb0a0bc05c3f8ba48eb27be7e0c4ba846dbe
Ironblade1 <83993262+Ironblade1@users.noreply.github.com>
2023-02-01 19:57:50 +0530

    Time: 3 ms (83.39%), Space: 7.2 MB (43.04%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -13,7 +13,7 @@ public:
 
     string gcdOfStrings(string str1, string str2) {
         if(str1+str2 == str2+str1){
-            return str1.substr(0,__gcd(str1.size(),str2.size()));
+            return str1.substr(0,hcf(str1.size(),str2.size()));
         }
         return "";
     }
