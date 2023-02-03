4d29113c9faa1886b8d8b3a96c5ae4c734b8fced
Ironblade1 <83993262+Ironblade1@users.noreply.github.com>
2023-02-03 22:32:17 +0530

    Time: 30 ms (55.91%), Space: 23.2 MB (6.32%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -20,17 +20,44 @@ public:
         return dp[ind][n] = max(take,notTake);
     }
     
+    int solveTab(vector<int> & slices, int k){
+        vector<vector<int>> dp1(k+2,vector<int>(k+2,0));
+        vector<vector<int>> dp2(k+2,vector<int>(k+2,0));
+        
+        for(int i=k-2;i>=0;i--){
+            for(int n =1;n<=k/3;n++){
+                int take = slices[i] + dp1[i+2][n-1];
+                int notTake= 0 + dp1[i+1][n];
+                dp1[i][n] = max(take,notTake);
+            }
+        }
+        int case1 = dp1[0][k/3];
+        
+        for(int i=k-1;i>=1;i--){
+            for(int n =1;n<=k/3;n++){
+                int take = slices[i] + dp1[i+2][n-1];
+                int notTake= 0 + dp1[i+1][n];
+                dp1[i][n] = max(take,notTake);
+            }
+        }
+        int case2 = dp1[1][k/3];
+        
+        return max(case1,case2);
+    }
+    
     int maxSizeSlices(vector<int>& slices) {
         int k = slices.size();
         // int case1 = solve(0,k-2,slices,k/3);
         // int case2 = solve(1,k-1,slices,k/3);
         // return max(case1,case2);
         
-        vector<vector<int>> dp1(k,vector(k,-1));
-        int case1 = solveMem(0,k-2,slices,k/3,dp1);
-        vector<vector<int>> dp2(k,vector(k,-1));
-        int case2 = solveMem(1,k-1,slices,k/3,dp2);
-        return max(case1,case2);
+        // vector<vector<int>> dp1(k,vector(k,-1));
+        // int case1 = solveMem(0,k-2,slices,k/3,dp1);
+        // vector<vector<int>> dp2(k,vector(k,-1));
+        // int case2 = solveMem(1,k-1,slices,k/3,dp2);
+        // return max(case1,case2);
+        
+        return solveTab(slices,k);
     }
     
 };
