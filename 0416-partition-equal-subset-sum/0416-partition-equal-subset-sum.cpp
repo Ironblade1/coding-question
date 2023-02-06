6e1bb4386b6574291ba3436ac8ad906b3a05372c
Ironblade1 <83993262+Ironblade1@users.noreply.github.com>
2023-02-06 20:35:17 +0530

    Time: 410 ms (36.91%), Space: 95.9 MB (17.79%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -25,6 +25,27 @@ public:
         return dp[ind][target] = inc or exc;
     }
     
+    bool solveTab(vector<int> &nums,int total){
+        int n = nums.size();
+        int t = total/2;
+        vector<vector<int>> dp(n+1,vector<int>(t+1,0));
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            for(int target = 0;target<=t;target++){
+                bool inc =0;
+                if(target-nums[i]>=0)
+                     inc = dp[i+1][target-nums[i]];
+                bool exc = dp[i+1][target];
+                dp[i][target] = inc or exc;
+            }
+        }
+        return dp[0][t];
+    }
+    
     bool canPartition(vector<int>& nums) {
         int total =0;
         for(int i:nums){
@@ -36,7 +57,9 @@ public:
         
         // return solve(0,nums,target);
         
-        vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
-        return solveMem(0,nums,target,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
+        // return solveMem(0,nums,target,dp);
+        
+        return solveTab(nums,total);
     }
 };
