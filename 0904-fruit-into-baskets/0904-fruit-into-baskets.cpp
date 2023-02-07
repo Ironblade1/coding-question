0c4d6b88bed26538ac62b4766400a5035a4052e6
Ironblade1 <83993262+Ironblade1@users.noreply.github.com>
2023-02-07 23:07:54 +0530

    Time: 205 ms (39.85%), Space: 71.2 MB (48.83%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -34,19 +34,19 @@ public:
     }
 
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int> count;
-        int i,j;
+        unordered_map<int,int> count; // hash map to store the frequency of the elements in the subarray
+        int i,j;  // variables i and j are used to keep track of the subarray boundaries
         for(i=0,j=0;j<fruits.size();j++){
-            count[fruits[j]]++;
+            count[fruits[j]]++;  // increase the count of the current fruit
             if(count.size()>2){
                 count[fruits[i]]--;
-                if(count[fruits[i]] ==0){
-                    count.erase(fruits[i]);
+                if(count[fruits[i]] ==0){   // reduce the count of the first fruit and remove it from the map if its count becomes 0
+                    count.erase(fruits[i]);  // move the start of the subarray to the right
                 }
                 i++;
             }
         }
-        return j-i;
+        return j-i;    // return the length of the longest subarray with at most two unique elements
 
 
 
