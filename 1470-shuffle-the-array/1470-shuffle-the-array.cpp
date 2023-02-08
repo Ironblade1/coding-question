51d688f1e87c18539b5bd75426cb90b5cd6cb7f4
Ironblade1 <83993262+Ironblade1@users.noreply.github.com>
2023-02-08 11:45:42 +0530

    Time: 4 ms (65.82%), Space: 9.9 MB (31.58%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -7,8 +7,7 @@ public:
         for(int i=0;i<n;i++){
             v1.push_back(nums[i]);
             v2.push_back(nums[i+n]);
-        }
-        for(int i=0;i<n;i++){
+        
             ans[2*i] = v1[i];
             ans[2*i+1] = v2[i];
         }
