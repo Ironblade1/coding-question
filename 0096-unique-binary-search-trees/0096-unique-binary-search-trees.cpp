b477e67dafecc0e4ae737c4db5a0828a0afd0128
Ironblade1 <83993262+Ironblade1@users.noreply.github.com>
2023-02-24 23:06:06 +0530

    Time: 0 ms (100.00%), Space: 6 MB (79.81%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -23,11 +23,27 @@ public:
         return dp[n]=ans;
     }
     
+    int solveTab(int n){
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){ // i is number of node
+            
+            for(int j=1;j<=i;j++){ // j is root node
+                dp[i] += dp[j-1] * dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+    
     int numTrees(int n) {
         // return solve(n);
         
-        vector<int> dp(n+1,-1);
-        return solveMem(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return solveMem(n,dp);
+        
+        return solveTab(n);
         
     }
 };
