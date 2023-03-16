b69df3ab05c22037611b388f750aa3e4e05e48ad
Ironblade1 <83993262+Ironblade1@users.noreply.github.com>
2023-03-16 17:50:11 +0530

    Time: 25 ms (57.25%), Space: 26.3 MB (51.00%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void Mapping(vector<int>& inorder,unordered_map<int,int> &mp){
+        for(int i=0;i<inorder.size();i++){
+            mp[inorder[i]]=i;
+        }
+    }
+    
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int index,int inorderStart,int inorderEnd,int n , unordered_map<int,int>& mp){
+        if(index>=n || inorderStart>inorderEnd) return NULL;
+        
+        int element = preorder[index++];
+        int position = mp[element];
+        TreeNode* root = new TreeNode(element);
+        
+        root->left = solve(preorder,inorder,index,inorderStart,position-1,n,mp);
+        root->right = solve(preorder,inorder,index+position-inorderStart,position+1,inorderEnd,n,mp);
+        
+        return root;
+        
+        
+    }
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        unordered_map<int,int> mp;
+        Mapping(inorder,mp);
+        for(int i=0;i<n;i++){
+            cout<<mp[inorder[i]]<<",";
+        }
+        TreeNode* ans = solve(preorder,inorder,0,0,n-1,n,mp);
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
