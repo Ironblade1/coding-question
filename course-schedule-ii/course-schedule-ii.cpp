078fa95918d00f01eef621f1a6b44f04c8198040
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-06-15 23:41:32 +0530

    Time: 27 ms (41.85%) | Memory: 15.9 MB (7.58%) - LeetSync

---
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool dfs(int node, unordered_map<int,list<int>> &mp, vector<bool>& visi, stack<int>& st,vector<bool>& dfsvisi){
+        visi[node]=1;
+        dfsvisi[node] = 1;
+        for(auto nbr:mp[node]){
+            if(!visi[nbr])
+                dfs(nbr,mp,visi,st,dfsvisi);
+            if(visi[nbr] && dfsvisi[nbr]) return true;
+        }
+        st.push(node);
+        dfsvisi[node]=false;
+        return false;
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,list<int>> mp;
+        for(auto p:prerequisites){
+            mp[p[1]].push_back(p[0]);
+        }
+        stack<int> st;
+        vector<int> ans;
+        vector<int> res;
+        vector<bool> dfsvisi(numCourses,0);
+        vector<bool> visi(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            if(!visi[i]){
+                bool isCyclic = dfs(i,mp,visi,st,dfsvisi);
+                if(isCyclic) return res; //empty vector
+            }  
+            
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
