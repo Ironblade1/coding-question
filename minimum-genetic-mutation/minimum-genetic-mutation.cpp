317254acebb3bc2a4ca3ee2117b2122f15f3810f
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-06-18 23:14:42 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (99.77%) - LeetSync

--- minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-    int minMutation(string start, string end, vector<string>& bank) {
-        queue<string>q;
-        unordered_set<string>visited;
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        queue<string> q;
+        unordered_set<string> visited;
+
+        int cnt = 0;
+        q.push(startGene);
+        visited.insert(startGene);
         
-        q.push(start);
-        visited.insert(start);
-        
-        int count = 0;
-        
-        while(!q.empty())
-        {
-            int n = q.size();
-            for(int i=0;i<n;i++)
-            {
+        while (!q.empty()) {
+            int s = q.size();
+            for (int i = 0; i < s; i++) {
                 string node = q.front();
                 q.pop();
-                
-                if(node == end)
-                {
-                    return count;
-                }
-                
-                for(char ch :"ACGT")
-                {
-                    for(int j=0;j<node.size();j++)
-                    {
-                        string adjNode = node;
-                        adjNode[j] = ch;
-                       if(!visited.count(adjNode) && find(bank.begin(),bank.end(),adjNode)!= bank.end())
-            {
-                q.push(adjNode);
-                visited.insert(adjNode);
-            }
+                if (node == endGene) return cnt;
+
+                for (int j = 0; j < node.size(); j++) {
+                    char originalChar = node[j];
+
+                    for (char c : {'A', 'C', 'G', 'T'}) {
+                        if (c == originalChar) continue;
+
+                        node[j] = c;
+                        if (visited.count(node) == 0 && find(bank.begin(), bank.end(), node) != bank.end()) {
+                            q.push(node);
+                            visited.insert(node);
+                        }
                     }
+
+                    node[j] = originalChar; // Restore the original character
                 }
             }
-             count++;
+            cnt++;
         }
-       return -1; 
+
+        return -1;
     }
 };
