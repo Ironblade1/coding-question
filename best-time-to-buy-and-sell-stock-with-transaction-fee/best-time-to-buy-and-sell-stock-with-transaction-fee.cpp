d92d8e9a953d1bad2f8250a4bf3ac3ad61d25af9
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-06-22 20:47:55 +0530

    Time: 379 ms (5.34%) | Memory: 126.5 MB (5.73%) - LeetSync

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -27,10 +27,31 @@ public:
         return dp[ind][buy] = profit;
     }
 
+    int solveTab(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy) {
+                    profit = max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
+                } else {
+                    profit = max(prices[i] - fee + dp[i + 1][1], dp[i + 1][0]);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+
+
     int maxProfit(vector<int>& prices, int fee) {
         // return solve(prices,fee, 0, 1); // recursive 
 
         vector<vector<int>> dp(prices.size()+1, vector<int>(3,-1));
-        return solveMem(prices, fee, 0 , 1, dp);
+        // return solveMem(prices, fee, 0 , 1, dp);
+
+        return solveTab(prices, fee);
     }
 };
