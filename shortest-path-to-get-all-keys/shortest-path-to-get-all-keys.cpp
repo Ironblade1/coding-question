19849243d046a4982cb0c652a5661c763931b045
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-06-29 13:59:00 +0530

    Time: 1004 ms (5.12%) | Memory: 106.9 MB (5.12%) - LeetSync

--- shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.cpp
+++ shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.cpp
@@ -1,39 +1,32 @@
-#include <vector>
-#include <string>
-#include <unordered_set>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
     int shortestPathAllKeys(vector<string>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        unordered_set<char> keySet{'a', 'b', 'c', 'd', 'e', 'f'};
-        unordered_set<char> lockSet{'A', 'B', 'C', 'D', 'E', 'F'};
+        pair<int,int> start;
+        string keyStr = "";
+        int cntKey = 0;
 
-        int keyCount = 0;
-        pair<int, int> start;  // (i,j)
-        string keyStr;
+        unordered_set<char> keySet = {'a','b','c','d','e','f'};
+        unordered_set<char> lockSet = {'A','B','C','D','E','F'};
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '@') {
-                    start = make_pair(i, j);
-                } else if (keySet.count(grid[i][j])) {
-                    keyCount++;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == '@'){
+                    start = {i,j};
                 }
+                if(keySet.count(grid[i][j])) cntKey++;
             }
         }
 
-        queue<pair<pair<int, int>, string>> q;
-        q.push(make_pair(start, keyStr));
-        unordered_set<string> visited;
-        vector<vector<int>> directions{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+        queue<pair<pair<int,int>, string>> q;
+        q.push({start, keyStr});
 
-        auto addAllDirections = [&](int i, int j, const string& keyStr) {
-            for (const auto& d : directions) {
+        unordered_set<string> visi;
+        vector<vector<int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
+
+        auto addAllDirections = [&](int i, int j, string& keyStr){
+            for(auto& d:dir){
                 int r = i + d[0];
                 int c = j + d[1];
 
@@ -46,48 +39,43 @@ public:
 
         int steps = 0;
 
-        while (!q.empty()) {
-            int levelSize = q.size();
-            for (int l = 0; l < levelSize; l++) {
-                pair<pair<int, int>, string> state = q.front();
+        while(!q.empty()){
+            int level = q.size();
+            for(int l=0; l<level; l++){
+                auto f = q.front();
                 q.pop();
+                int i = f.first.first;
+                int j = f.first.second;
+                keyStr = f.second;
 
-                int i = state.first.first;
-                int j = state.first.second;
-                const string& keyStr = state.second;
-
-                if (visited.count(to_string(i) + "," + to_string(j) + "," + keyStr) || grid[i][j] == '#') {
-
+                if(visi.count(to_string(i) + "," + to_string(j) + "," + keyStr) || grid[i][j] == '#'){
                     continue;
                 }
-
                 char c = grid[i][j];
-                if (lockSet.count(c)) {
-                    if (keyStr.find(tolower(c)) == string::npos){
-                        continue;
-                    } else {
+
+                if(lockSet.count(c)){
+                    if(keyStr.find(tolower(c)) != string::npos){
                         addAllDirections(i, j, keyStr);
                     }
-                } else if (keySet.count(c)) {
-                    if (keyStr.find(tolower(c)) == string::npos){
-                        string newKeyStr = keyStr + c;
-                        if (newKeyStr.size() == keyCount) {
-                            return steps;
-                        }
-                        addAllDirections(i, j, newKeyStr);
-                    } else {
+                }
+                else if(keySet.count(c)){
+                    if(keyStr.find(c) != string::npos){
                         addAllDirections(i, j, keyStr);
                     }
-                } else {
+                    else{
+                        keyStr += c;
+                        if(keyStr.length() == cntKey) return steps;
+                        addAllDirections(i, j, keyStr);
+                    }
+                }
+                else{
                     addAllDirections(i, j, keyStr);
                 }
 
-                visited.insert(to_string(i) + "," + to_string(j) + "," + keyStr);
+                visi.insert(to_string(i) + "," + to_string(j) + "," + keyStr);
             }
-
             steps++;
         }
-
         return -1;
     }
 };
