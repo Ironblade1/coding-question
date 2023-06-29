b37209fcd3f418bccb4dbff7be31dc9bfd48d22e
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-06-30 00:34:46 +0530

    Time: 7 ms (32.71%) | Memory: 5.9 MB (46.02%) - LeetSync

---
+++ sqrtx/sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long int mySqrt(int x) {
+        int s = 0;
+        int e = x;
+        long long int ans = -1;
+        long long int mid = s + (e-s)/2;
+        while (s<=e){
+            long long int  square = mid*mid;
+            if (square==x){
+                return mid;
+            }
+            if (square<x){
+                 ans = mid;
+                s = mid+1;
+                
+            }
+            else{
+                e = mid -1;
+            }
+            mid = s+ (e-s)/2;
+        }
+        return ans;
+    }
+};
