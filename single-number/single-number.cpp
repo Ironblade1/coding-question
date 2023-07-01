b08dc9cbee734b85c97529929cd9019d91cb33c1
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-07-01 12:42:06 +0530

    Time: 20 ms (75.35%) | Memory: 16.8 MB (67.56%) - LeetSync

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-        for(int i:nums){
+        for(int& i:nums){
             ans = ans ^ i;
         }
         return ans;
