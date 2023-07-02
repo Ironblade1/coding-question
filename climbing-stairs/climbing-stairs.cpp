3721913915937cdcde0ab38a945efb432777f9ed
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-07-02 23:27:09 +0530

    Time: 4 ms (13.13%) | Memory: 6.1 MB (56.23%) - LeetSync

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -9,8 +9,20 @@ public:
         return dp[i] = solve(i+1,n,dp) + solve(i+2,n,dp);
     }
 
+    int solveTab(int n){
+        vector<int> dp(n+2,0);
+        dp[n] = 1;
+
+        for(int i=n-1;i>=0;i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        return dp[0];
+    }
+
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(0,n, dp);
+        // vector<int> dp(n+1,-1);
+        // return solve(0,n, dp);
+
+        return solveTab(n);
     }
 };
