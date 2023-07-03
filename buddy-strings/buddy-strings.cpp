c7e4f68e531462d05fa2b60fa48da9fbe26284de
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-07-03 14:16:01 +0530

    Time: 5 ms (45.91%) | Memory: 6.9 MB (85.37%) - LeetSync

--- buddy-strings/buddy-strings.cpp
+++ buddy-strings/buddy-strings.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
-        // if(s.length()!=goal.length()) return 0;
+        if(s.length()!=goal.length()) return 0;   // s and goal have different length 
         int n = s.length();
         unordered_map<int,int> mp;
         for(int i=0;i<n;i++){
             mp[s[i]] = i;
         }
 
-        if(s==goal && mp.size() != goal.size()) return 1;  // in this edge case s and goal are indentical and they have duplicate element
+        if(s==goal && mp.size() != goal.size()) return 1;  // in this edge case s and goal are indentical and they have duplicate element eg (s = "aa" and goal = "aa" if swap between 0 and 1 index then s and goal are identical)
 
         int i=0, j=0 ;
-        bool flag = 0;
+        bool flag = 0;   // using flag for doing only 1 swap
         while(i<n && j<n && !flag){
             if(s[i]!=goal[j]){
                 char c = goal[j];
@@ -19,6 +19,7 @@ public:
                 swap(s[i],s[ind]);
                 flag = 1;
             }
+            if(flag) break;
             i++;
             j++;
         }
