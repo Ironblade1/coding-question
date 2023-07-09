427fa0b103a6e03ef2bf539667e65c8530901c67
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-07-09 22:25:02 +0530

    Time: 862 ms (5.03%) | Memory: 127.3 MB (5.03%) - LeetSync

--- substring-with-largest-variance/substring-with-largest-variance.cpp
+++ substring-with-largest-variance/substring-with-largest-variance.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+
+    int solve(int i, int j, string s){
+        int c1 = 0; // counter for i;
+        int c2 = 0; // counter for j;
+        int r = 0;
+        for(auto c:s){
+            if(c==i) c1++;
+            if(c==j) c2++;
+            
+            if(c2>c1){
+                c2=0;
+                c1 = 0;
+            }
+            if(c1>0 && c2>0){
+                r = c1-c2;
+            }
+        }
+            return r;
+    }
+
     int largestVariance(string s) {
         unordered_set<char> set;
         for(auto c:s){
@@ -14,6 +34,9 @@ public:
         for(auto i:set){
             for(auto j:set){
                 if(i==j) continue;
+
+                int val = max(solve(i,j,s), solve(j,i,s));
+                ans = max(ans, val);
 
                 for(int k=0;k<2;k++){
                     int c1 = 0; // counter for i;
