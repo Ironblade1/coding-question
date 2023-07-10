92d66276c336b19109d08e300253710df1b68cba
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-07-10 12:54:51 +0530

    Time: 287 ms (69.34%) | Memory: 144.4 MB (96.58%) - LeetSync

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -13,19 +13,43 @@ class Solution {
 public:
     int mini = INT_MAX;
 
-    void solve(TreeNode* root, int cnt){
+    void dfs(TreeNode* root, int cnt){
         if(root==NULL) return ;
         if(!root->left && !root->right){
             mini = min(mini, cnt);
             return ;
         }
-        solve(root->left,cnt+1);
-        solve(root->right,cnt+1);
+        dfs(root->left,cnt+1);
+        dfs(root->right,cnt+1);
+    }
+
+    int bfs(TreeNode* root){
+        queue<TreeNode*> q;
+        q.push(root);
+        int ans = 0;
+
+        while(!q.empty()){
+            int s = q.size();
+
+            for(int i=0;i<s;i++){
+                TreeNode* front = q.front();
+                q.pop();
+
+                if(!front->left && !front->right) return ans;
+
+                if(front->left){
+                    q.push(front->left);
+                }
+                if(front->right) q.push(front->right);
+            }
+            ans++;
+        }
+        return ans;
     }
 
     int minDepth(TreeNode* root) {
         if(!root) return 0;
-        solve(root,1);
+        dfs(root,1);
         return mini;
     }
 };
