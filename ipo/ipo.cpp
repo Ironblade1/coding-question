109db4f60ca94e0e68b9cf6cd76168d38f1e08cc
Rohit Kumar <83993262+Ironblade1@users.noreply.github.com>
2023-07-13 16:47:42 +0530

    Time: 967 ms (5.00%) | Memory: 82 MB (30.37%) - LeetSync

---
+++ ipo/ipo.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    static bool cmp(pair<int,int>& a, pair<int,int>& b){
+        if(a.first == b.first){
+            return a.second > b.second;
+        }
+        return a.first < b.first;
+    }
+
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int,int>> proj;
+        for(int i=0;i<profits.size();i++){
+            proj.push_back({capital[i],profits[i]});
+        }
+
+        sort(proj.begin(),proj.end(),cmp);
+
+        for(auto p:proj){
+            cout<<p.first<<","<<p.second<<endl;
+        }
+        priority_queue<int> pq;
+        int n = proj.size();
+        int i = 0;
+        while (k > 0) {
+            while(i<n && w >= proj[i].first) {
+                pq.push(proj[i].second);
+                i++;
+            }
+            if(!pq.empty()){
+                w+= pq.top();
+                pq.pop();
+            }
+            k--;
+        }
+
+        return w;
+    }
+};
